7f047c13f1217a39fb415d3f5b96e8d315818ea6
Riggs <riggs9162@gmx.de>
2024-12-26 05:44:54 +0100

    Create cl_fonts.lua

---
+++ lua/vanguard/core/cl_fonts.lua
@@ -0,0 +1,59 @@
+surface.CreateFont("Vanguard.Title", {
+    font = "Arial",
+    size = 48,
+    weight = 500
+})
+
+surface.CreateFont("Vanguard.SubTitle", {
+    font = "Arial",
+    size = 32,
+    weight = 500
+})
+
+surface.CreateFont("Vanguard.Fontello", {
+    font = "fontello",
+    size = 24,
+    weight = 500
+})
+
+surface.CreateFont("Vanguard.FontelloViewer", {
+    font = "Arial",
+    size = 24,
+    weight = 500
+})
+
+surface.CreateFont("Vanguard.Button.Small", {
+    font = "Arial",
+    size = 16,
+    weight = 500
+})
+
+surface.CreateFont("Vanguard.Button", {
+    font = "Arial",
+    size = 24,
+    weight = 500
+})
+
+surface.CreateFont("Vanguard.Button.Large", {
+    font = "Arial",
+    size = 32,
+    weight = 500
+})
+
+surface.CreateFont("Vanguard.Body.Small", {
+    font = "Arial",
+    size = 16,
+    weight = 500
+})
+
+surface.CreateFont("Vanguard.Body", {
+    font = "Arial",
+    size = 18,
+    weight = 500
+})
+
+surface.CreateFont("Vanguard.Body.Large", {
+    font = "Arial",
+    size = 24,
+    weight = 500
+})
